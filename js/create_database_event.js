953921d9988d5a9267f66e07518df15f5fe64bf7
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2023-02-28 12:04:08 -0600

    - Created an HTML page for performing the Firebase OAuth so USACE members can login and create new historical events - Created an HTML page for submitting data to add a new historical event to the database - Created the create_database_event.js script to handle all of the input validation for submitting a new historical event

---
+++ js/create_database_event.js
@@ -0,0 +1,12 @@
+//import {getDatabase, ref, set} from "https://mregan-capstone-default-rtdb.firebaseio.com/HISTORICAL_EVENTS/.json/?";
+
+function writeDatabaseEvt(title, desc, start, end, img){
+    //const db = getDatabase();
+    //console.log(db);
+    console.log(title + " " + desc + " " + start);
+}
+
+
+function submitForm(form){
+    console.log("Blerg");
+}
