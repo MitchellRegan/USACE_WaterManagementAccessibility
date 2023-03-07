4acf21c6e80b209a4352c7f95b1631a60e04d36f
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2023-03-07 13:13:44 -0600

    - Condensed all of the event time inputs for create_event.html, and also included a timezone field - Fixed the button for submitting the input data to create_database_event.writeDatabaseEvt(), since that was having some problems - Writing some functions in create_database_event.js to start formatting the data, and also trying to figure out how to write it to our Firebase database

--- js/create_database_event.js
+++ js/create_database_event.js
@@ -1,12 +1,21 @@
-//import {getDatabase, ref, set} from "https://mregan-capstone-default-rtdb.firebaseio.com/HISTORICAL_EVENTS/.json/?";
-
-function writeDatabaseEvt(title, desc, start, end, img){
-    //const db = getDatabase();
-    //console.log(db);
-    console.log(title + " " + desc + " " + start);
+async function queryDB() {
+    const dBQuery = new Request("https://mregan-capstone-default-rtdb.firebaseio.com/HISTORICAL_EVENTS/.json/?");
+    let dB = await fetch(dBQuery);
+    dB = await dB.json();
+    return dB;
 }
 
 
-function submitForm(form){
-    console.log("Blerg");
+function toEpochTime(edate, timezone){
+    console.log(edate, timezone)
+    let d = new Date();
+    d.toUTCString()
+}
+
+
+async function writeDatabaseEvt(title, desc, startDate, endDate, timezone, img){
+    //const db = queryDB();
+    //console.log(db);
+    
+    let evtStart = toEpochTime(startDate, timezone);
 }
