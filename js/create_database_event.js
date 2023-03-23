a040415fdfe26787e21a8f392de416bde473bf73
MitchRegan <mitchell.p.regan@gmail.com>
2023-03-23 12:17:07 -0500

    - Modified usace_login.html to automatically forward to the create_event.html page once logged-in. This fixes an error for Firefox where the login would happen, but would just stay on the same page - Modified create_database_event.js to perform data validation on the user inputs and then clear all of the input fields once the event is created

--- js/create_database_event.js
+++ js/create_database_event.js
@@ -17,22 +17,79 @@ function findSiteOptions(inputText){
 
 
 /**
+* Function to check for any potential errors or malicious code injection with the user-given inputs.
+* Returns True if all checks are valid.
+*/
+function validateData(title, desc, sites, sDate, eDate, img){
+    //Checking for empty input fields for required data
+    if(title == ''){
+        alert("Please provide a title for this event.");
+        return false;
+    }
+    if(desc == ''){
+        alert("Please provide a description for the event.");
+        return false;
+    }
+    if(sites == ''){
+        alert("Please provide at least one water site that was affected by this event.");
+        return false;
+    }
+    if(sDate == '' || eDate == ''){
+        alert("Please provide a starting date and ending date for when this event occurred.");
+        return false;
+    }
+    
+    //Preventing JSON injection
+    var combinedStr = "" + title + desc + sites + sDate + eDate + img;
+    if(combinedStr.includes('{') || combinedStr.includes('}')){
+        alert("The characters '{' and '}' are not allowed.");
+        return false;
+    }
+    
+    //If all checks have been passed, we're safe to upload to the database
+    return true;
+}
+
+
+/**
+* Function to clear all of the input fields on create_database_event.html after a successful upload.
+*/
+function clearInputFields(){
+    document.getElementById('evtTitle').value = '';
+    document.getElementById('evtDesc').value = '';
+    document.getElementById('siteSearch').value = '';
+    document.getElementById('evtStart').value = '';
+    document.getElementById('evtEnd').value = '';
+    document.getElementById('evtTZ').value = '';
+    document.getElementById('evtImg').value = '';
+}
+
+
+/**
 * Function called from create_event.html on form submission.
 */
 async function writeDatabaseEvt(title, desc, sites, startDate, endDate, timezone, img){
-    if(title == '' || desc == '' || sites == '' || startDate == '' || endDate == ''){
-        alert("Please fill out all required fields.");
+    //Validating the inputs first
+    if(!validateData(title, desc, sites, startDate, endDate, img)){
         return;
     }
     
+    //Converting the start and end dates to Epoch time in seconds
     let sd = new Date(startDate);
     let ed = new Date(endDate);
     let evtStart = (sd.getTime() / 1000) + (3600 * timezone);
     let evtEnd = (ed.getTime() / 1000) + (3600 * timezone);
+    //Making sure the end date actually comes after the start date
     if(evtStart > evtEnd){
         let placeholder = evtStart;
         evtStart = evtEnd;
         evtEnd = placeholder;
+    }
+    //Making sure neither of the dates are in the future
+    let now = new Date();
+    if(evtEnd > (now.getTime() / 1000)){
+        alert("The time stamps for this event cannot be in the future.");
+        return;
     }
     
     let evtjson = {
@@ -43,8 +100,6 @@ async function writeDatabaseEvt(title, desc, sites, startDate, endDate, timezone
         "EVT_STARTDATE":evtStart,
     };
     
-    console.log(evtjson);
-    
     fetch('https://mregan-capstone-default-rtdb.firebaseio.com/HISTORICAL_EVENTS.json', {
         method: 'POST',
         headers: {
@@ -53,47 +108,12 @@ async function writeDatabaseEvt(title, desc, sites, startDate, endDate, timezone
         body: JSON.stringify(evtjson)
     })
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        clearInputFields();
+        alert("Event " + title + " has been created and uploaded to the database.");
+    })
     .catch(e => {
         console.error(e);
         alert("An error occurred. Event not uploaded to database.");
     })
-}
-
-
-function testWriteDb(token){
-    console.log("===\n" + token + "\n---");
-    fetch('https://mregan-capstone-default-rtdb.firebaseio.com/HISTORICAL_EVENTS.json', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'auth': token
-        },
-        body: JSON.stringify({
-            "EVT_DESC":"Test description",
-            "EVT_ENDDATE":"Test end",
-            "EVT_IMAGE":undefined,
-            "EVT_STARTDATE":"Test start",
-            "EVT_TITLE":"Test Title"
-        })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(e => {
-        console.error(e);
-        alert("An error occurred. Event not uploaded to database.");
-    })
-    
-    /**import {getDatabase, ref, push, child, update} from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
-    const tdb = getDatabase();
-    push(ref(tdb, "HISTORICAL_EVENTS/"),{
-        "EVT_DESC":"Test description",
-        "EVT_ENDDATE":"Test end",
-        "EVT_IMAGE":undefined,
-        "EVT_STARTDATE":"Test start",
-        "EVT_TITLE":"Test Title"
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(e => console.error(e))*/
 }
