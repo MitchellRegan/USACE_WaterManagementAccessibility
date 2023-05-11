46c15d48a605a02c8a8f50c840d5c51c633d1df5
BadWolf22 <matthewpostel02@gmail.com>
2023-05-11 14:18:32 -0500

    Fixed bug: Causes error when firebase library is not included in the webpage

--- js/firebaseSettings.js
+++ js/firebaseSettings.js
@@ -1,22 +1,26 @@
+var uiConfig, firebaseConfig;
+
 // FirebaseUI config.
-var uiConfig = {
-    signInSuccessUrl: './create_event.html',
-    signInFlow: "popup",
-    signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+if (typeof firebase !== 'undefined') { // Only setup uiConfig if the firebase library is included.
+    uiConfig = {
+        signInSuccessUrl: './create_event.html',
+        signInFlow: "popup",
+        signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 
-        //These are other available options that we aren't currently using
-        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        //firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-        //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-    ]
-};
+            //These are other available options that we aren't currently using
+            //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            //firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+        ]
+    };
+}
 
-const firebaseConfig = {
+firebaseConfig = {
     apiKey: "AIzaSyAr7SQASmasb7pk7E3OHhuewJoY76CcJ30",
     authDomain: "mregan-capstone.firebaseapp.com",
     databaseURL: "https://mregan-capstone-default-rtdb.firebaseio.com",
